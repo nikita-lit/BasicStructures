f2c5b344390c994e6c45dee5607120a91ed2f140
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-02-12 15:56:55 +0200

    Update ÜlesandedIF.py

--- ÜlesandedIF.py
+++ ÜlesandedIF.py
@@ -1,26 +1,42 @@
-print("Kino")
+# 1. Juku
 
-nimi = input("Mis on sinu nimi? ")
+# print("Kino")
 
-if nimi.isupper() and nimi.lower() == "juku":
-    print("Lähme kinno")
+# nimi = input("Mis on sinu nimi? ")
 
-    try:
-        vanus = int(input("Kui vana sa oled? "))
+# if nimi.isupper() and nimi.lower() == "juku":
+#     print("Lähme kinno")
 
-        if vanus < 0 or vanus > 100:
-            pilet = "!!!"
-        elif vanus < 6:
-            pilet = "Tasuta"
-        elif vanus >= 14:
-            pilet = "Lastepilet"
-        elif vanus >= 65:
-            pilet = "Täospilet"
-        elif vanus <= 100:
-            pilet = "Sooduspilet"
+#     try:
+#         vanus = int(input("Kui vana sa oled? "))
 
-        print(pilet)
-    except Exception as e:
-        print(f"ERROR {e}")
+#         if vanus < 0 or vanus > 100:
+#             pilet = "!!!"
+#         elif vanus < 6:
+#             pilet = "Tasuta"
+#         elif vanus <= 14:
+#             pilet = "Lastepilet"
+#         elif vanus <= 65:
+#             pilet = "Täospilet"
+#         elif vanus <= 100:
+#             pilet = "Sooduspilet"
+
+#         print(pilet)
+#     except Exception as e:
+#         print(f"ERROR {e}")
+# else:
+#     print("Ma olen hõivatud")
+
+# 2. Pinginaabrid
+nimi1 = input("Sisesta esimese inimese nimi: ")
+nimi2 = input("Sisesta teise inimese nimi: ")
+
+if nimi1.isalpha() and nimi2.isalpha():
+    nimi1 = nimi1.lower()
+    nimi2 = nimi2.lower()
+
+    if (nimi1 == "maksim" and nimi2 == "nikita") or (nimi1 == "nikita" and nimi2 == "maksim"):
+        print("Nad olevad pinginaaber")
+
 else:
-    print("Ma olen hõivatud")
+    print("ERROR")
