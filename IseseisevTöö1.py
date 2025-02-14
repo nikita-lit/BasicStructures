0222d7e5b2eea1c921fe8d8a8aefe482dcf2307e
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-02-14 13:45:11 +0200

    iseseisev töö update

--- IseseisevTöö1.py
+++ IseseisevTöö1.py
@@ -5,35 +5,50 @@
 from math import * # import oli vale
 
 # --------------------------------------
-print("Ruudu karakteristikud")
-a = float(input("Sisesta ruudu külje pikkus => ")) # input() peab olema konverteeritud float'iks
-S= a ** 2
-print("Ruudu pindala", S)
-P = 4 * a
-print("Ruudu ümbermõõt", P)
-di = a * sqrt(2) # "sqr(2)" ei ole korrektne, õige on "sqrt(2)"
+try:
+    print("Ruudu karakteristikud")
+    a = float(input("Sisesta ruudu külje pikkus => ")) # input() peab olema konverteeritud float'iks
+    S= a ** 2
+    print(f"Ruudu pindala: {S}")
+    P = 4 * a
+    print(f"Ruudu ümbermõõt: {P}")
+    di = a * sqrt(2) # "sqr(2)" ei ole korrektne, õige on "sqrt(2)"
 
-print("Ruudu diagonaal", round(di,2))
+    print("Ruudu diagonaal", round(di,2))
+except Exception as e:
+    print(f"ERROR: {e}")
+
 print()
-print("Ristküliku karakteristikud")
 
-b = float(input("Sisesta ristküliku 1. külje pikkus => ")) # input() peab olema konverteeritud float'iks
-c = float(input("Sisesta ristküliku 2. külje pikkus => ")) # input() peab olema konverteeritud float'iks
-S = b * c
+try:
+    print("Ristküliku karakteristikud")
 
-print("Ristküliku pindala", S) # Sõnumis olid puudu jutumärgid ja vale süntaks
-P = 2 * (b + c)  # Puudu oli korrutamise operaator
-print("Ristküliku ümbermõõt", P)
-di = sqrt( b ** 2 + c ** 2 ) # Korrektne valem diagonaali jaoks on "b**2 + c**2"
+    b = float(input("Sisesta ristküliku 1. külje pikkus => ")) # input() peab olema konverteeritud float'iks
+    c = float(input("Sisesta ristküliku 2. külje pikkus => ")) # input() peab olema konverteeritud float'iks
+    S = b * c
 
-print("Ristküliku diagonaal", round(di)) # Sulgude puudavad lõpus
+    print("Ristküliku pindala", S) # Sõnumis olid puudu jutumärgid ja vale süntaks
+    P = 2 * (b + c)  # Puudu oli korrutamise operaator
+    print("Ristküliku ümbermõõt", P)
+    di = sqrt( b ** 2 + c ** 2 ) # Korrektne valem diagonaali jaoks on "b**2 + c**2"
+
+    print("Ristküliku diagonaal", round(di)) # Sulgude puudavad lõpus
+except Exception as e:
+    print(f"ERROR: {e}")
+
 print()
-print("Ringi karakteristikud")
 
-r = float(input("Sisesta ringi raadiusi pikkus => ")) # input() peab olema konverteeritud float'iks
-d = 2 * r # Puudu oli korrutamise operaator
-print("Ringi läbimõõt", d)
-S = pi * r ** 2 # pi() -> pi ("pi" ei ole funktsiooni); Valem pindala jaoks oli vale, õige on "pi * r**2"
-print("Ringi pindala", round(S))
-C = 2 * pi * r # Valem ringjoone pikkuse jaoks oli vale, õige on "2 * pi * r"'""
-print("Ringjoone pikkus", round(C)) # Sulgude puudavad lõpus
+try:
+    print("Ringi karakteristikud")
+
+    r = float(input("Sisesta ringi raadiusi pikkus => ")) # input() peab olema konverteeritud float'iks
+    d = 2 * r # Puudu oli korrutamise operaator
+    print("Ringi läbimõõt", d)
+
+    S = pi * r ** 2 # pi() -> pi ("pi" ei ole funktsiooni); Valem pindala jaoks oli vale, õige on "pi * r**2"
+    print("Ringi pindala", round(S))
+
+    C = 2 * pi * r # Valem ringjoone pikkuse jaoks oli vale, õige on "2 * pi * r"'""
+    print("Ringjoone pikkus", round(C)) # Sulgude puudavad lõpus
+except Exception as e:
+    print(f"ERROR: {e}")
