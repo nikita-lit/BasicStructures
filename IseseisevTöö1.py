49817975f04407963daae9d06454a8128c14b717
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-02-14 14:16:03 +0200

    Update IseseisevTöö1.py

--- IseseisevTöö1.py
+++ IseseisevTöö1.py
@@ -8,13 +8,17 @@ from math import * # import oli vale
 try:
     print("Ruudu karakteristikud")
     a = float(input("Sisesta ruudu külje pikkus => ")) # input() peab olema konverteeritud float'iks
-    S= a ** 2
-    print(f"Ruudu pindala: {S}")
-    P = 4 * a
-    print(f"Ruudu ümbermõõt: {P}")
-    di = a * sqrt(2) # "sqr(2)" ei ole korrektne, õige on "sqrt(2)"
 
-    print("Ruudu diagonaal", round(di,2))
+    if a > 0:
+        S= a ** 2
+        print(f"Ruudu pindala: {S}")
+        P = 4 * a
+        print(f"Ruudu ümbermõõt: {P}")
+        di = a * sqrt(2) # "sqr(2)" ei ole korrektne, õige on "sqrt(2)"
+
+        print("Ruudu diagonaal", round(di,2))
+    else:
+        print("Ruudu külje pikkus ei saa olla negatiivne!!!")
 except Exception as e:
     print(f"ERROR: {e}")
 
@@ -25,14 +29,19 @@ try:
 
     b = float(input("Sisesta ristküliku 1. külje pikkus => ")) # input() peab olema konverteeritud float'iks
     c = float(input("Sisesta ristküliku 2. külje pikkus => ")) # input() peab olema konverteeritud float'iks
-    S = b * c
 
-    print("Ristküliku pindala", S) # Sõnumis olid puudu jutumärgid ja vale süntaks
-    P = 2 * (b + c)  # Puudu oli korrutamise operaator
-    print("Ristküliku ümbermõõt", P)
-    di = sqrt( b ** 2 + c ** 2 ) # Korrektne valem diagonaali jaoks on "b**2 + c**2"
+    if a > 0 and c > 0:
+        S = b * c
 
-    print("Ristküliku diagonaal", round(di)) # Sulgude puudavad lõpus
+        print("Ristküliku pindala", S) # Sõnumis olid puudu jutumärgid ja vale süntaks
+        P = 2 * (b + c)  # Puudu oli korrutamise operaator
+        print("Ristküliku ümbermõõt", P)
+        di = sqrt( b ** 2 + c ** 2 ) # Korrektne valem diagonaali jaoks on "b**2 + c**2"
+
+        print("Ristküliku diagonaal", round(di)) # Sulgude puudavad lõpus
+    else:
+        print("Külje pikkus ei saa olla negatiivne!!!")
+
 except Exception as e:
     print(f"ERROR: {e}")
 
@@ -42,13 +51,17 @@ try:
     print("Ringi karakteristikud")
 
     r = float(input("Sisesta ringi raadiusi pikkus => ")) # input() peab olema konverteeritud float'iks
-    d = 2 * r # Puudu oli korrutamise operaator
-    print("Ringi läbimõõt", d)
 
-    S = pi * r ** 2 # pi() -> pi ("pi" ei ole funktsiooni); Valem pindala jaoks oli vale, õige on "pi * r**2"
-    print("Ringi pindala", round(S))
+    if r > 0:
+        d = 2 * r # Puudu oli korrutamise operaator
+        print("Ringi läbimõõt", d)
 
-    C = 2 * pi * r # Valem ringjoone pikkuse jaoks oli vale, õige on "2 * pi * r"'""
-    print("Ringjoone pikkus", round(C)) # Sulgude puudavad lõpus
+        S = pi * r ** 2 # pi() -> pi ("pi" ei ole funktsiooni); Valem pindala jaoks oli vale, õige on "pi * r**2"
+        print("Ringi pindala", round(S))
+
+        C = 2 * pi * r # Valem ringjoone pikkuse jaoks oli vale, õige on "2 * pi * r"'""
+        print("Ringjoone pikkus", round(C)) # Sulgude puudavad lõpus
+    else:
+        print("Ringi raadiusi pikkus ei saa olla negatiivne!!!")
 except Exception as e:
     print(f"ERROR: {e}")
