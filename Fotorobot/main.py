3132b525f576bf8f7c1ee7711bc3e3fcaae7c1c6
Opilane TTHK <nikitalitvinenko28@gmail.com>
2025-05-23 11:46:06 +0300

    Update main.py

--- Fotorobot/main.py
+++ Fotorobot/main.py
@@ -1,37 +1,48 @@
 import customtkinter as ctk
-from tkinter import simpledialog, Canvas, messagebox, colorchooser
+from tkinter import messagebox, colorchooser, filedialog
+from CTkColorPicker import *
 from PIL import Image, ImageTk
 import pygame
+import os
 
 #pygame.mixer.init()
 #pygame.mixer.music.load("music.mp3")
 
 def play_music():
-    pygame.mixer.music.play(-1)  
+    #pygame.mixer.music.play(-1)  
+    pass
+
 def stop_music():
-    pygame.mixer.music.stop()
+    #pygame.mixer.music.stop()
+    pass
 
 ctk.set_appearance_mode("dark")
+ctk.set_default_color_theme("green")
 
-FACE_PARTS_LIST = ["base", "forehead", "eyes", "nose", "mouth"]
 FACE_PARTS = {
-    "base": {
+    #müts
+    "Nägu": {
+        "Puudub": None,
         "Face1": "fotorobot/images/base1.png",
         "Face2": "fotorobot/images/base2.png",
     },
-    "forehead": {
-        "Eears1": "fotorobot/images/forehead1.png",
-        "Eears2": "fotorobot/images/forehead2.png",
+    "Kõrvad": {
+        "Puudub": None,
+        "Ears1": "fotorobot/images/forehead1.png",
+        "Ears2": "fotorobot/images/forehead2.png",
     },
-    "eyes": {
+    "Silmad": {
+        "Puudub": None,
         "Eyes1": "fotorobot/images/eyes1.png",
         "Eyes2": "fotorobot/images/eyes2.png",
     },
-    "nose": {
+    "Nina": {
+        "Puudub": None,
         "Nose1": "fotorobot/images/nose1.png",
         "Nose2": "fotorobot/images/nose2.png",
     },
-    "mouth": {
+    "Suu": {
+        "Puudub": None,
         "Mouth1": "fotorobot/images/mouth1.png",
         "Mouth2": "fotorobot/images/mouth2.png",
     },
@@ -42,9 +53,12 @@ canvas = None
 pictures = {}
 objects = {}
 parts = {}
-color_label = None
+bg_label = None
 WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 500
+WINDOW_HEIGHT = 550
+
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
 
 def create_window():
     window = ctk.CTk()
@@ -53,106 +67,183 @@ def create_window():
     window.title("Näo koostaja nuppudega")
     window.iconbitmap("icon.ico")
 
-    global canvas
-    canvas = ctk.CTkCanvas(window, width=400, height=400, bg="white")
-    canvas.configure(highlightthickness=0)
-    canvas.pack(side="right", padx=10, pady=10)
-    
-    toggle_part("base", "fotorobot/images/base1.png", 200, 200)
-    parts["base"] = True
-    
+    create_canvas()
     create_buttons()
+    
     window.mainloop()
 
+# --------------------------------------
 def toggle_part(name, file, x, y):
     global canvas
-    if parts.get(name):
-        canvas.delete(objects[name])
-        parts[name] = False
+    if not file:
+        pictures[name] = None
+        if name in objects:
+            canvas.delete(objects[name])
+        parts[name] = None
     else:
-        img = Image.open(file).convert("RGBA").resize((400, 400), 1)
+        if parts.get(name):
+            canvas.delete(objects[name])
+        
+        img = Image.open(file).convert("RGBA").resize((CANVAS_WIDTH, CANVAS_HEIGHT), 1)
         tk_image = ImageTk.PhotoImage(img)
         pictures[name] = tk_image
         objects[name] = canvas.create_image(x, y, image=tk_image)
-        parts[name] = True
+        parts[name] = file
         
+def update_part(part_name, option_name):
+    file = FACE_PARTS[part_name][option_name]
+    toggle_part(part_name, file, CANVAS_WIDTH/2, CANVAS_HEIGHT/2)
+      
+def random_parts():
+    pass
+      
+# -------------------------------------- 
 def save_face():
-    filename = simpledialog.askstring("Salvesta pilt", "Sisesta faili nimi (ilma laiendita):")
+    dialog_filename = ctk.CTkInputDialog(text="Sisesta faili nimi (ilma laiendita):", title="Salvesta pilt")
+    filename = dialog_filename.get_input()
     if not filename:
         return
     
-    image_result = Image.new("RGBA", (400, 400), (255, 255, 255, 255))
+    image_result = Image.new("RGBA", (CANVAS_WIDTH, CANVAS_HEIGHT), (255, 255, 255, 255))
     
-    for name in FACE_PARTS_LIST:
-        if parts.get(name):
-            file_path = {
-                "base": "fotorobot/images/base1.png",
-                "forehead": "fotorobot/images/forehead1.png",
-                "eyes": "fotorobot/images/eyes1.png",
-                "nose": "fotorobot/images/nose1.png",
-                "mouth": "fotorobot/images/mouth1.png",
-            }.get(name)
-            if file_path:
-                part = Image.open(file_path).convert("RGBA").resize((400, 400), 1)
-                image_result.alpha_composite(part)
+    for face_part in FACE_PARTS:
+        if parts.get(face_part):
+            part = Image.open(parts[face_part]).convert("RGBA").resize((CANVAS_WIDTH, CANVAS_HEIGHT), 1)
+            image_result.alpha_composite(part)
             
-    image_result.save(f"{filename}.png")
+    image_result.save(f"fotorobot/pildid/{filename}.png")
     messagebox.showinfo("Info", f"Pilt {filename}.png salvestatud!")
-            
+    
+def clear_canvas():
+    for face_part in FACE_PARTS:
+        update_part(face_part, "Puudub")
+       
+# --------------------------------------   
 bg_rect = None
+bg_image = None
             
 def choose_color():
-    global color_label, bg_rect
-    color_code = colorchooser.askcolor(title="Choose a color")[1]
-    
+    global bg_label, bg_rect
+    pick_color = AskColor(title="Valige värv")
+    color_code = pick_color.get()
+
     if bg_rect:
         canvas.delete(bg_rect)
     
     if color_code:
-        color_label.configure(text=f"Selected Color: {color_code}", bg_color=color_code)
+        bg_label.configure(text=f"Valitud värv: {color_code}")
         bg_rect = canvas.create_rectangle(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, fill=color_code)
         canvas.tag_lower(bg_rect)
+        
+def choose_background_img():
+    global bg_label, bg_rect, bg_image
+    filepath = filedialog.askopenfile(title="Valige fail", filetypes=[("PNG pildid", "*.png")])
+    if filepath and os.path.exists(filepath.name):
+        if bg_rect:
+            canvas.delete(bg_rect)
+            
+        img = Image.open(filepath.name).convert("RGBA").resize((CANVAS_WIDTH, CANVAS_HEIGHT), 1)
+        tk_image = ImageTk.PhotoImage(img)
+        
+        bg_rect = canvas.create_image(CANVAS_WIDTH/2, CANVAS_HEIGHT/2, image=tk_image)
+        bg_image = tk_image
+        canvas.tag_lower(bg_rect)
+        
+        bg_label.configure(text=f"Valitud fail: {os.path.basename(filepath.name)}")
 
-def create_buttons():
+def clear_bg():
+    global bg_image, bg_label, bg_rect
+    if bg_rect:
+        canvas.delete(bg_rect)
+        
+    bg_image = None
+    bg_label.configure(text=f"Valitud värv: #ffffff")
+        
+# --------------------------------------     
+button_config = {
+    "width": 150, "height": 40,
+    "font": ("Segoe UI Emoji", 16),
+    "text_color": "white",
+    "corner_radius": 10,
+}
+
+dropdowns = []
+
+def create_faceparts(pframe):
+    global dropdowns
+    
+    frame = ctk.CTkFrame(pframe)
+    frame.pack(side="top", padx=5)
+    
+    dframe = ctk.CTkFrame(frame)
+    dframe.pack(side="left", padx=5, pady=5)
+    
+    for part_name in FACE_PARTS:
+        label = ctk.CTkLabel(dframe, text=part_name.capitalize(), font=("Segoe UI", 14))
+        label.pack()
+
+        options = list(FACE_PARTS[part_name].keys())
+        var = ctk.StringVar(value=options[0])
+
+        dropdown = ctk.CTkOptionMenu(
+            dframe, values=options,
+            command=lambda opt, p=part_name: update_part(p, opt),
+            variable=var
+        )
+        dropdown.pack(padx=5, pady=5)
+        dropdowns.append(dropdown)
+        
+    bframe = ctk.CTkFrame(frame)
+    bframe.pack(side="right", padx=5)
+    
+    clear_button = ctk.CTkButton(bframe, text="Puhasta", command=clear_canvas, **button_config)
+    clear_button.pack(side="top", pady=5, padx=5)
+            
+def create_canvas():
     frame = ctk.CTkFrame(window)
-    frame.pack(side="left", padx=10, pady=10)
-    button_config = {
-        "width": 150, "height": 40,
-        "font": ("Segoe UI Emoji", 16),
-        "fg_color": "#4CAF50",
-        "text_color": "white",
-        "corner_radius": 10,
-    }
-
-    # ctk.CTkLabel(frame, text="Vali näoosad", bg_color="gray50", font= ("Segoe UI Emoji", 50), corner_radius=10).pack(pady=5)
-    # ctk.CTkButton(frame, text="🧢 Otsmik", command=lambda: toggle_part("forehead", "fotorobot/images/forehead1.png", 200, 200), **button_config).pack(pady=5)
-    # ctk.CTkButton(frame, text="👀 Silmad", command=lambda: toggle_part("eyes", "fotorobot/images/eyes1.png", 200, 200), **button_config).pack(pady=5)
-    # ctk.CTkButton(frame, text="👃 Nose", command=lambda: toggle_part("nose", "fotorobot/images/nose1.png", 200, 200), **button_config).pack(pady=5)
-    # ctk.CTkButton(frame, text="👄 Suu", command=lambda: toggle_part("mouth", "fotorobot/images/mouth1.png", 200, 200), **button_config).pack(pady=5)
-
-    for part in FACE_PARTS_LIST:
-        part_values = list(FACE_PARTS[part].keys())
-        selected_value = part_values[0]
-        dropdown = ctk.CTkOptionMenu(frame, values=part_values, variable=selected_value)
-        dropdown.pack(pady=10)
-
-    global color_label
-    color_label = ctk.CTkLabel(frame, text="Selected Color: None", width=30, height=4)
-    color_label.pack(pady=20)
-
-    color_button = ctk.CTkButton(frame, text="Pick a Color", command=choose_color)
-    color_button.pack(pady=10)
-
+    frame.pack(side="right", padx=(0, 25))
+    
+    global canvas
+    canvas = ctk.CTkCanvas(frame, width=CANVAS_WIDTH, height=CANVAS_HEIGHT, bg="white")
+    canvas.configure(highlightthickness=0)
+    canvas.pack()
+    
     button = ctk.CTkButton(frame, text="Salvesta nägu", command=save_face, **button_config)
     button.pack(pady=10)
 
+def create_background_buttons(pframe):
+    frame = ctk.CTkFrame(pframe)
+    frame.pack(padx=5, pady=5)
+    
+    global bg_label
+    bg_label = ctk.CTkLabel(frame, text="Valitud värv: #ffffff", width=30, height=4)
+    bg_label.pack(pady=5)
+
+    color_button = ctk.CTkButton(frame, text="Taustvärv", command=choose_color)
+    color_button.pack(side="left", pady=5, padx=(5, 2))
+    
+    clear_button = ctk.CTkButton(frame, text="Puhasta", command=clear_bg, width=25)
+    clear_button.pack(side="right", pady=5, padx=(2, 5))
+    
+    image_button = ctk.CTkButton(frame, text="Taustpilt", command=choose_background_img)
+    image_button.pack(side="right", pady=5, padx=(2, 2))
+
+def create_buttons():
+    frame = ctk.CTkFrame(window)
+    frame.pack(side="left", padx=5, pady=5)
+
+    ctk.CTkLabel(frame, text="Vali näoosad", font= ("Segoe UI Emoji", 50), corner_radius=10).pack(pady=5)
+    
+    create_faceparts(frame)
+    create_background_buttons(frame)
+
     frame_mus = ctk.CTkFrame(frame)
-    frame_mus.pack(side="bottom", padx=10, pady=10)
+    frame_mus.pack(side="bottom", padx=5, pady=5, fill=ctk.BOTH, expand=True)
 
-    button_mus = ctk.CTkButton(frame_mus, text="Mängi muusikat", command=play_music, fg_color="#4CAF50")
-    button_mus.pack(side="left", padx=10, pady=10)
+    button_mus = ctk.CTkButton(frame_mus, text="Mängi muusikat", command=play_music)
+    button_mus.pack(side="left", padx=(5, 2), pady=5, fill=ctk.BOTH, expand=True)
 
-    button_stop_mus = ctk.CTkButton(frame_mus, text="Peata muusika", command=stop_music, fg_color="#4CAF50")
-    button_stop_mus.pack(side="left", padx=10, pady=10)
+    button_stop_mus = ctk.CTkButton(frame_mus, text="Peata muusika", command=stop_music)
+    button_stop_mus.pack(side="right", padx=(2, 5), pady=5, fill=ctk.BOTH, expand=True)
     
 create_window()
